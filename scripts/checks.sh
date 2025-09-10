483c8c720ec597bb618493cff6f2f751543d7a85
Daniel Spofford <868014+danielspofford@users.noreply.github.com>
2025-09-10 14:52:37 -0600

    ignore RUSTSEC-2023-0071

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -13,7 +13,7 @@ if [ -z "$CI" ]; then
         echo "cargo install cargo-audit"
         exit 1
     fi
-    cargo audit --ignore RUSTSEC-2023-0071
+    cargo audit
 fi
 
 # Check for cargo-llvm-cov (skip check in CI as it's installed by workflow)
